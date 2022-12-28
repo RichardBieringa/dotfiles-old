2293d1bf3353ee92039da3c6a72be7e779b002be
Richard Bieringa <RichardBieringa@gmail.com>
2022-12-28 14:59:08 +0100

    updated nvim config

---
+++ dotfiles/nvim/.config/nvim/lua/richardbieringa/options.lua
@@ -0,0 +1,71 @@
+local options = {
+	-- use system clipboard
+    clipboard = "unnamedplus",
+
+    -- line numbers + relative line numbers
+    number = true,
+    relativenumber = true,
+
+    -- whitespace settings
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+
+    -- case insensitive searching UNLESS /C or capital in search
+    ignorecase = true,
+    smartcase = true,
+
+    -- automatic indenting on new lines
+    smartindent = true,
+
+    -- disables line wrapping
+    wrap = false,
+
+    -- removes backup/swap files
+    swapfile = false,
+    backup = false,
+
+    -- enables long running undos
+    undodir = "~/.vim/undodir",
+    undofile = true,
+
+    -- search settings
+    hlsearch = false,
+    incsearch = true,
+
+    -- 24 bit colors
+    termguicolors = true,
+
+    -- scrolling settings
+    scrolloff = 8,
+    sidescrolloff = 8,
+
+    -- updatetime (CursorHold event)
+    updatetime = 50,
+
+    -- always draw signs (e.g. LSP diag)
+    signcolumn = "yes",
+
+    -- more space for command line messages
+    cmdheight = 2,
+
+    -- enables mouse in all modes
+    mouse = "a",
+
+    -- disables showing vim mode
+    showmode = false,
+
+    -- always show tabs (collections of windows)
+    showtabline = 2,
+
+    -- split position
+    splitbelow = true,
+    splitright = true,
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+
