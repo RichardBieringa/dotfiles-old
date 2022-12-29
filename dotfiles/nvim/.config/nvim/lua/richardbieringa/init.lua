f7965f39d99dd55c0fc12ee0aad325fc4aa4b719
Richard Bieringa <RichardBieringa@gmail.com>
2022-12-29 14:51:46 +0100

    removed nvim config

--- dotfiles/nvim/.config/nvim/lua/richardbieringa/init.lua
+++
@@ -1,9 +0,0 @@
--- vim / nvim options
-require("richardbieringa.options")
-
--- custom keymap settings
--- note: extension specific settings in after/lua dir
-require("richardbieringa.keymap")
-
--- configure plugins
-require("richardbieringa.plugins")
